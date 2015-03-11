3b69e7874bee3fee5b3934df3031597c84f145e9
James Awesome <awesome@shopkeep.com>
2015-03-10 22:58:38 -0400

    Convert to using class methods

--- lib/random_yiff.rb
+++ lib/random_yiff.rb
@@ -5,24 +5,22 @@ require "random_yiff/version"
 
 class RandomYiff
 
-  attr_reader :post
+  class << self
+    def post
+      JSON.load(Net::HTTP.get(post_uri))
+    end
 
-  def initialize
-    @post = JSON.load(Net::HTTP.get(post_uri))
-  end
+    def file_uri
+      URI(post['file_url'])
+    end
 
-  def file_uri
-    URI(post['file_url'])
-  end
+    def file
+      Net::HTTP.get(file_uri)
+    end
 
-  def file
-    Net::HTTP.get(file_uri)
-  end
-
-  private
-
-  def post_uri
-    res = Net::HTTP.get_response(URI("https://e621.net/post/random"))
-    URI(res['location'] + '?format=json')
+    def post_uri
+      res = Net::HTTP.get_response(URI("https://e621.net/post/random"))
+      URI(res['location'] + '?format=json')
+    end
   end
 end
--- spec/random_yiff_spec.rb
+++ spec/random_yiff_spec.rb
@@ -6,7 +6,6 @@ describe RandomYiff do
   random_post_json = JSON.load(File.read("spec/fixtures/random_post.json"))
   random_post_url = "https://e621.net/post/show/141529/azazial-breasts-cellphone-claws-clothed-clothing-f?format=json"
   random_image_url = "https://static1.e621.net/data/d7/74/d7745463ef599bb21702fd173c103d41.jpg"
-  subject(:yiff) { RandomYiff.new }
 
   before do
     stub_request(:get, "https://e621.net/post/random").to_return(random_post_response)
@@ -14,27 +13,27 @@ describe RandomYiff do
     stub_request(:get, random_image_url)
   end
 
-  describe "#post_uri" do
+  describe ".post_uri" do
     it "Returns a uri object for a random e621 post" do
-      expect(yiff.send(:post_uri)).to eq(URI(random_post_url))
+      expect(RandomYiff.post_uri).to eq(URI(random_post_url))
     end
   end
 
-  describe "#post" do
+  describe ".post" do
     it "Returns a json object for a random post from e621" do
-      expect(yiff.post).to eq(random_post_json)
+      expect(RandomYiff.post).to eq(random_post_json)
     end
   end
 
-  describe "#file_uri" do
+  describe ".file_uri" do
     it "Returns a uri object for a random furry pr0n file" do
-      expect(yiff.file_uri).to eq(URI(random_image_url))
+      expect(RandomYiff.file_uri).to eq(URI(random_image_url))
     end
   end
 
-  describe "#file" do
+  describe ".file" do
     it "Download random furry pr0n" do
-      yiff.file
+      RandomYiff.file
       expect(WebMock).to have_requested(:get, random_image_url)
     end
   end
