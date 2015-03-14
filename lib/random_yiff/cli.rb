4f80a020a986c4d1f510fc71f7d558f5861aa690
James Awesome <james@wesome.nyc>
2015-03-14 15:36:42 -0400

    Rough out ascii and download commands

--- lib/random_yiff/cli.rb
+++ lib/random_yiff/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'launchy'
+require 'asciiart'
 
 module RandomYiff
   class Cli < Thor
@@ -9,6 +10,24 @@ module RandomYiff
         Launchy.open(yiff.file_url)
       end
     end
+
+    desc 'ascii', 'Print furry pr0n as ascii'
+    def ascii
+      while yiff = RandomYiff::E621.new
+        break unless yiff.file_ext == 'flv'
+      end
+      puts AsciiArt.new(yiff.file_url).to_ascii_art(color: true, width: 160)
+    end
+
+    desc 'download', 'Download furry pr0n'
+    def download
+      RandomYiff::E621.new do |yiff|
+        File.open("./#{yiff.file_name}", 'w') do |f|
+          f.write(yiff.file)
+        end
+      end
+    end
+
     default_task :open
   end
 end
